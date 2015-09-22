ed40897f08347564e03eb11df1ec2d95ac751dd7
ThrowawayGithub <jkorotzer@wustl.edu>
2015-09-22 15:50:49 -0500

    yaaaaaas

--- Pivit/AddGoalViewController.swift
+++ Pivit/AddGoalViewController.swift
@@ -74,17 +74,23 @@ class AddGoalViewController: UIViewController, UITextFieldDelegate, UIImagePicke
     }
     
     func keyboardWillShow(sender: NSNotification) {
-        self.view.frame.origin.y -= 180
+        self.view.frame.origin.y -= 200
     }
     
     func keyboardWillHide(sender: NSNotification) {
-        self.view.frame.origin.y += 180
+        self.view.frame.origin.y += 200
     }
     
     //MARK: - Outlet Funcs
 
-    @IBAction func saveNewGoal(sender: UIBarButtonItem) {
-        
+    @IBAction func goBack(sender: UIBarButtonItem) {
+        goalNameTextField.resignFirstResponder()
+        goalAmountTextField.resignFirstResponder()
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+    @IBAction func done(sender: UIBarButtonItem) {
         var goalName: String?
         var goalMoneyNecessary: Double?
         
@@ -100,8 +106,11 @@ class AddGoalViewController: UIViewController, UITextFieldDelegate, UIImagePicke
         
         handler.saveNewGoal(goalName: goalName!, totalMoneyNeeded: goalMoneyNecessary!, picture: picture!)
         
-        performSegueWithIdentifier("goalSubmitted", sender: self)
+        goalNameTextField.resignFirstResponder()
+        goalAmountTextField.resignFirstResponder()
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
+    
     
     //MARK: - UIPickerController Handling
     
